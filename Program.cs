e3161762c3e197451fb644396172eaeb79cfbe6d
ASUS <ASUS@MIRALI>
2025-06-27 18:00:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mira_bd
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! I am mira");
+            Console.Read();
+        }
+    }
+}
