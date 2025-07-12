5699cf2bff85d46857066e19b8f40197222cab6a
Mira136 <mirap7365@gmail.com>
2025-07-12 19:53:54 +0530

    Tutorial - 1 Program

---
+++ Pattern1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mira_bd
+{
+    class Pattern1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
