7dc1aa11dd2b2eaa1995763c6ca0adb89a18b5b8
Mira136 <mirap7365@gmail.com>
2025-07-13 22:31:39 +0530

    Array program

---
+++ SortArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mira_bd
+{
+    class SortArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+
+            Console.WriteLine("Enter 5 integer values:");
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(n);
+            Console.WriteLine("\nSorted array in ascending order:");
+
+            foreach (int num in n)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
