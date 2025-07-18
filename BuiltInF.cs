d0e02d18bafc1d74f38d140c08b6c85253e0c36b
Mira136 <mirap7365@gmail.com>
2025-07-18 13:10:41 +0530

    program

---
+++ BuiltInF.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace mira_bd
+{
+    class BuiltInF
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 5, 7, 3, 4, 10, 20, 11, 12 };
+
+            Console.WriteLine("Max num: " + a.Max());
+            Console.WriteLine("Min num: " + a.Min());
+
+            Console.WriteLine();
+
+            var reverse = a.Reverse();
+            Console.WriteLine("Reversed num is: ");
+            foreach (int i in reverse)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+
+            Array.Sort(a);
+            Console.WriteLine("\nSorted num is: ");
+            foreach (int i in a)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+
+        }
+    }
+}
